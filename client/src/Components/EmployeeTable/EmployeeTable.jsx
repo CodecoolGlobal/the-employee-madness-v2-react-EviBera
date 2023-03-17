a81f32f95d17ea0e1d20a1059b7ea817a4c06d09
EviBera <beraevi@gmail.com>
2023-03-17 11:14:19 +0100

    add options to select rearrange base

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,41 +1,68 @@
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
+import { useState } from "react";
 
-const EmployeeTable = ({ employees, onDelete }) => (
-  <div className="EmployeeTable">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Level</th>
-          <th>Position</th>
-          <th />
-        </tr>
-        <tr>
-          <th></th>
-          <th><input placeholder="Search by level"></input></th>
-          <th><input placeholder="Search by position"></input></th>
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee._id}>
-            <td>{employee.name}</td>
-            <td>{employee.level}</td>
-            <td>{employee.position}</td>
-            <td>
-              <Link to={`/update/${employee._id}`}>
-                <button type="button">Update</button>
-              </Link>
-              <button type="button" onClick={() => onDelete(employee._id)}>
-                Delete
-              </button>
-            </td>
+const EmployeeTable = ({ employees, onDelete }) => {
+
+  const [level, setLevel] = useState('');
+  const [position, setPosition] = useState('');
+  const [rearranger, setRearranger] = useState('');
+
+  const handleClick = (event) => {
+    console.log(event.target.value);
+    setRearranger(event.target.value);
+  }
+
+  return (
+    <div className="EmployeeTable">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Position</th>
+            <th>Rearrange employees by</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+          <tr>
+            <th></th>
+            <th><input placeholder="Search by level" onChange={(event) => setLevel(event.target.value)} /></th>
+            <th><input placeholder="Search by position" onChange={(event) => setPosition(event.target.value)} /></th>
+            <th>
+              <form onClick={handleClick}>
+                <select name="arrangeOptions" id="arrangeOptions">
+                  <option value="firstName">First name</option>
+                  <option value="middleName">Middle name</option>
+                  <option value="lastName">Last name</option>
+                  <option value="position">Position</option>
+                  <option value="level">Level</option>
+                </select>
+              </form>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees
+            .filter(employee => employee.level.toLowerCase().includes(level.toLowerCase()))
+            .filter(employee => employee.position.toLowerCase().includes(position.toLowerCase()))
+            .map((employee) => (
+              <tr key={employee._id}>
+                <td>{employee.name}</td>
+                <td>{employee.level}</td>
+                <td>{employee.position}</td>
+                <td>
+                  <Link to={`/update/${employee._id}`}>
+                    <button type="button">Update</button>
+                  </Link>
+                  <button type="button" onClick={() => onDelete(employee._id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div >
+  );
+}
 
 export default EmployeeTable;
