c3ea4b1a8d3ea8b948a511dee9f3da4c15a2fe97
EviBera <beraevi@gmail.com>
2023-03-23 10:15:52 +0100

    add arrangement features (middle name)

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -6,11 +6,78 @@ const EmployeeTable = ({ employees, onDelete }) => {
 
   const [level, setLevel] = useState('');
   const [position, setPosition] = useState('');
-  const [rearranger, setRearranger] = useState('');
+  const [rearranger, setRearranger] = useState('')
 
   const handleClick = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     setRearranger(event.target.value);
+  }
+
+  const sortEmployees = (a, b) => {
+    if (rearranger === "level") {
+      if (a.level < b.level) {
+        return -1
+      }
+      if (a.level > b.level) {
+        return 1
+      }
+      return 0
+    } 
+
+    if (rearranger === "position"){
+      if (a.position < b.position) {
+        return -1
+      }
+      if (a.position > b.position) {
+        return 1
+      }
+      return 0
+    }
+
+    if (rearranger === "firstName") {
+      if (a.name < b.name) {
+        return -1
+      }
+      if (a.name > b.name) {
+        return 1
+      }
+      return 0
+    }
+
+    if (rearranger === "middleName") {
+      let middleNameA = a.name.split(" ");
+      middleNameA.pop();
+      middleNameA.shift();
+
+      let middleNameB = b.name.split(" ");
+      middleNameB.pop();
+      middleNameB.shift();
+
+      if (middleNameA.length === 0) {
+        return 1
+      }
+      if (middleNameB.length === 0) {
+        return -1
+      }
+
+      if (middleNameA < middleNameB) {
+        return -1
+      }
+      if (middleNameA > middleNameB) {
+        return 1
+      }
+      return 0
+    }
+
+    if (rearranger === "lastName") {
+      if (a.name.split(" ").pop() < b.name.split(" ").pop()) {
+        return -1
+      }
+      if (a.name.split(" ").pop() > b.name.split(" ").pop()) {
+        return 1
+      }
+      return 0
+    }
   }
 
   return (
@@ -42,6 +109,7 @@ const EmployeeTable = ({ employees, onDelete }) => {
         </thead>
         <tbody>
           {employees
+            .sort(sortEmployees)
             .filter(employee => employee.level.toLowerCase().includes(level.toLowerCase()))
             .filter(employee => employee.position.toLowerCase().includes(position.toLowerCase()))
             .map((employee) => (
