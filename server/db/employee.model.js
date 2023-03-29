af150d55d9a6ce343b7d9dad71282f3a73b678d7
EviBera <beraevi@gmail.com>
2023-03-29 10:39:49 +0200

    modify employee schema and add some initial data

--- server/db/employee.model.js
+++ server/db/employee.model.js
@@ -7,6 +7,16 @@ const EmployeeSchema = new Schema({
   name: String,
   level: String,
   position: String,
+  startingDate: {
+    type: Date,
+    default: Date.now,
+  },
+  currentSalary: Number,
+  desiredSalary: Number,
+  favColor: {
+    type: String,
+    default: "white"
+  },
   created: {
     type: Date,
     default: Date.now,
